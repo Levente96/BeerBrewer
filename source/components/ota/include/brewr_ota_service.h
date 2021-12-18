3ae69d827c1aa276827119dcb1f4c3b136f4e90c
Levente96 <levente.csoka96@gmail.com>
2021-12-18 15:56:28 +0100

    Initial version

---
+++ source/components/ota/include/brewr_ota_service.h
@@ -0,0 +1,26 @@
+#ifndef _BREWR_OTA_SERVICE_H_
+#define _BREWR_OTA_SERVICE_H_
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_event.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "esp_ota_ops.h"
+#include "esp_http_client.h"
+#include "esp_https_ota.h"
+
+extern const uint8_t server_cert_pem_start[] asm("_binary_ca_cert_pem_start");
+extern const uint8_t server_cert_pem_end[] asm("_binary_ca_cert_pem_end");
+
+extern esp_err_t _http_event_handler(esp_http_client_event_t *evt);
+void ota_task(void * pvParameter);
+
+#endif // _BREWR_OTA_SERVICE_H_
---
+++ source/main/main.c
@@ -0,0 +1,31 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_event.h"
+#include "esp_err.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+
+// Component headers
+#include "brewr_ota_service.h"
+
+httpd_handle_t server = NULL;
+
+void app_main()
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+	xTaskCreate(&ota_task, "ota_task", 8192, NULL, 5, NULL);
+
+    // ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &server));
+    // ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
+
+    // server = start_webserver();
+	
+}
